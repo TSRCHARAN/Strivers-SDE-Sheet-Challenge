86f6942c58bc8cc9374b511a2ab44e45b0469763
TSRC <20b91a5461@srkrec.ac.in>
2023-07-28 23:56:20 +0530

    Day - 23

---
+++ TREES/search_in_BST.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x)
+{
+    BinaryTreeNode<int>*cur = root;
+    while(cur)
+    {
+        if(cur->data==x)
+        {
+            return true;
+        }
+        else if(cur->data>x)
+        {
+            cur = cur->left;
+        }
+        else
+        {
+            cur = cur->right;
+        }
+    }
+    return false;
+}
